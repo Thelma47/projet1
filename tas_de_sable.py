d565fafae5d49e50dbaa849d9b821659cf3b458a
Thomas VIGNERON <thomas.vigneron@ens.uvsq.fr>
2022-02-09 16:13:33 +0100

    gggg

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,10 +1,10 @@
-#########################################
+########################################
 # groupe MIASHS 7
 # MAHMOUD SOW
 # GOREAU THELMA
 # VIGNERON THOMAS
 # https://github.com/Thelma47/projet1
-#########################################
+########################################
 
 import tkinter as tk
 import random
