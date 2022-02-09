a812a314fa6d5c92c0170155b44aac307d9d29b9
uvsq22104079 <91213934+uvsq22104079@users.noreply.github.com>
2022-02-09 16:15:05 +0100

    Update tas_de_sable.py

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,10 +1,18 @@
-#########################################
+########################################
 # groupe MIASHS 7
 # MAHMOUD SOW
-
 # GOREAU THELMA
 # VIGNERON THOMAS
 # https://github.com/Thelma47/projet1
-#########################################
-#aaaa
+########################################
 
+import tkinter as tk
+import random
+
+CANVAS_WIDTH, CANVAS_HEIGHT = 500, 600
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT, bg = "#000000")
+
+root.mainloop()
